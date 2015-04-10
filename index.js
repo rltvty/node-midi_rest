be8dbed7995bbbc46f4d36caa7b46a5847d0c69b
Eric Pinzur <ericpinzur@otherinbox.com>
2015-04-09 23:15:56 -0500

    added b-control

--- index.js
+++ index.js
@@ -1,7 +1,26 @@
 var base = require('rest_base');
 var mixer_control = require('./lib/mixer_control');
+var b_control = require('./lib/b_control');
 
+//mixer_control.assign_routes(base.app);
 
-mixer_control.assign_routes(base.app);
 
 base.start_server();
+
+//do something when app is closing
+process.on('exit', exitHandler.bind(null,{cleanup:true}));
+
+//catches ctrl+c event
+process.on('SIGINT', exitHandler.bind(null, {exit:true}));
+
+//catches uncaught exceptions
+process.on('uncaughtException', exitHandler.bind(null, {exit:true}));
+
+
+function exitHandler(options, err) {
+  mixer_control.closePort();
+  b_control.closePort();
+  if (options.cleanup) console.log('clean');
+  if (err) console.log(err.stack);
+  if (options.exit) process.exit();
+}
